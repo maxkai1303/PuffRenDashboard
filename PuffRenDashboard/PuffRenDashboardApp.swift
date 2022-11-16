36a922588c567b9b888a71010b000f0263cb2a00
Max <k1071303@gmail.com>
2022-11-16 23:22:19 +0800

    Create project

---
+++ PuffRenDashboard/PuffRenDashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  PuffRenDashboardApp.swift
+//  PuffRenDashboard
+//
+//  Created by Max Kai on 2022/11/16.
+//
+
+import SwiftUI
+
+@main
+struct PuffRenDashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
